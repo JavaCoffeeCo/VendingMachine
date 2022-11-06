024df2b95499910222a4acad79e6b9efba46c4f5
ehzrl <117248305+ehzrl@users.noreply.github.com>
2022-11-06 17:27:36 -0600

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,26 +1,17 @@
 package rewardsSystem;
 
 import javax.swing.*;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import java.io.*;
+
 
 public class GUI {
 	private static JLabel welcLabel;
-	//private static JLabel userLabel;
 	private static JTextField userText;
-	//private static JPasswordField passwordText;
-	//private static JButton button;
 	private static JLabel success;
-	//private static JLabel passwordLabel;
-	//private String status;
 
-	public GUI()
-	{
-		
-	}
+	// Default Constructor
+	public GUI(){}
 	
-	
+	// runGUI will display a message updating a user on their login status
 	public void runGUI(String s)
 	{
 		JPanel panel = new JPanel();
@@ -39,33 +30,6 @@ public class GUI {
 		welcLabel = new JLabel("Login successful");
 		welcLabel.setBounds(10, 10, 500, 50); // x y width height
 		panel.add(welcLabel);
-				
-		/*// Username
-		userLabel = new JLabel("Username");
-		userLabel.setBounds(10, 30, 80, 25); // x y width height
-		panel.add(userLabel);
-		
-		// User field
-		userText = new JTextField(20);
-		userText.setBounds(100, 30, 165, 25);
-		panel.add(userText);
-		*/
-		
-		/*// Password
-		passwordLabel = new JLabel("Password");
-		passwordLabel.setBounds(10, 60, 80, 25);
-		panel.add(passwordLabel);
-		
-		// Password field
-		passwordText = new JPasswordField();
-		passwordText.setBounds(100, 60, 165, 25);
-		panel.add(passwordText);
-		
-		button = new JButton("Enter");
-		button.setBounds(10, 90, 80, 25);
-		panel.add(button);
-		button.addActionListener(new GUI());
-		*/
 		
 		success = new JLabel(s);
 		success.setBounds(10, 50, 300, 50);
