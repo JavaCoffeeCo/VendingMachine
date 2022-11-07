1f0ec195f7eab06dcd5ad7645c02bbf319702256
ehzrl <117248305+ehzrl@users.noreply.github.com>
2022-11-06 18:25:13 -0600

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -7,8 +7,6 @@ import javax.swing.*;
 
 
 public class GUI {
-	private static JLabel welcLabel;
-	private static JTextField userText;
 	private static JLabel success;
 
 	// Default Constructor
@@ -29,11 +27,7 @@ public class GUI {
 		// Configure panel
 		panel.setLayout(null);
 		
-		// Intro
-		welcLabel = new JLabel("Login successful");
-		welcLabel.setBounds(10, 10, 500, 50); // x y width height
-		panel.add(welcLabel);
-		
+		// Print login message
 		success = new JLabel(s);
 		success.setBounds(10, 50, 300, 50);
 		panel.add(success);
